197ba338592df9c7a753814fb69b420016ff1abe
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-31 22:30:11 +0600

    home and error page added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import ErrorHandle from './components/ErrorHandle/ErrorHandle';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorHandle />,
+    children: [
+      {
+        path: '/',
+        element: <App/>
+      }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
