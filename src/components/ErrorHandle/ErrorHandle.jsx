2d16b82e45aa8e6f63b32f5abb9e49f2fdb95b42
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-31 23:23:03 +0600

    Error page finished

--- src/components/ErrorHandle/ErrorHandle.jsx
+++ src/components/ErrorHandle/ErrorHandle.jsx
@@ -1,11 +1,21 @@
+import { useRouteError } from "react-router-dom";
 import React from 'react';
 
 const ErrorHandle = () => {
+     const error = useRouteError();
+     console.error(error);
      return (
-          <div>
-               <h1>This is error page.</h1>
+          <div className="flex place-content-center my-40" id="error-page">
+               <div>
+                    <h1 className="text-5xl font-medium">Oops!😢😢</h1>
+                    <h3 className="text-2xl font-medium mt-8">Sorry, an unexpected error has occurred.</h3>
+                    <p className="text-xl mt-7">
+                         <i>{error.statusText || error.message}</i>
+                    </p>
+               </div>
           </div>
      );
 };
+
 
 export default ErrorHandle;
